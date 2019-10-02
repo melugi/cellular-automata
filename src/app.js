d7963d3e8ae1709f9a2de7ddccee96f146f514a2
Miguel Rios <mrios0495@gmail.com>
2019-10-02 13:31:43 -0400

    adds a second route to separate html serving from grid updating

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import Grid from "./Grid";
 import Express from 'express';
+import * as path from 'path';
 
 const app = Express();
 
+app.use('/public', Express.static(path.join(__dirname, '../public')));
+
 app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+app.get('/grid', function (request, response) {
   let grid = new Grid(200, 200);
   grid.initializeGrid();
 
