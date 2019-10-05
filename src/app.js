2de5a7a1c3967befee6d7a0eec6a8367f249f278
Miguel Rios <mrios0495@gmail.com>
2019-10-04 21:14:34 -0400

    adds a second route for updating the gol grid

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Express from 'express';
 import * as path from 'path';
 
 const app = Express();
+var grid;
 
 app.use('/public', Express.static(path.join(__dirname, '../public')));
 
@@ -11,10 +12,18 @@ app.get('/', function (request, response) {
 });
 
 app.get('/grid', function (request, response) {
-  let grid = new Grid(200, 200);
+  grid = new Grid(200, 200);
   grid.initializeGrid();
 
-  response.json(grid.stringify());
+  response.json(grid.htmlify());
+});
+
+app.put('/grid', function (request, response) {
+  if (typeof grid !== 'undefined') {
+    grid.update();
+  } else {
+    throw new Error('Grid is not initialized');
+  }
 });
 
 app.listen(3001, console.log("Listening on port 3001."));
