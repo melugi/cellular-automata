6ea9e8040f285f66ff5d4a473898f514d9884617
Miguel Rios <mrios0495@gmail.com>
2019-12-13 16:41:14 -0500

    Fixes spacing

--- test/Grid.test.js
+++ test/Grid.test.js
@@ -1,28 +1,28 @@
 import Grid from '../src/Grid';
 
 const testData = {
-    length: 3,
-    width: 3,
-    grid: [
-        [true, false, false],
-        [true, false, true],
-        [false, false, false]
-    ]
+  length: 3,
+  width: 3,
+  grid: [
+    [true, false, false],
+    [true, false, true],
+    [false, false, false]
+  ]
 };
 
 test('Grid Initilization', () => {
-    let grid1 = new Grid(testData.length, testData.width);
-    grid1.initializeGrid(testData.grid);
+  let grid1 = new Grid(testData.length, testData.width);
+  grid1.initializeGrid(testData.grid);
 
-    expect(grid1.mapToStateArray()).toEqual(testData.grid);
-})
+  expect(grid1.mapToStateArray()).toEqual(testData.grid);
+});
 
 test('Grid Equality', () => {
-    let grid1 = new Grid(testData.length, testData.width);
-    let grid2 = new Grid(testData.length, testData.width);
-    grid1.initializeGrid(testData.grid);
-    grid2.initializeGrid(testData.grid);
+  let grid1 = new Grid(testData.length, testData.width);
+  let grid2 = new Grid(testData.length, testData.width);
+  grid1.initializeGrid(testData.grid);
+  grid2.initializeGrid(testData.grid);
 
-    expect(grid1.mapToStateArray()).toEqual(grid2.mapToStateArray());
-    expect(JSON.stringify(grid1.mapToStateArray())).toEqual(JSON.stringify(grid2.mapToStateArray()));
+  expect(grid1.mapToStateArray()).toEqual(grid2.mapToStateArray());
+  expect(JSON.stringify(grid1.mapToStateArray())).toEqual(JSON.stringify(grid2.mapToStateArray()));
 });
