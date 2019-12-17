599f1f91ade8e20fdeb7db735a8b8c8550e06fb2
Miguel Rios <mrios0495@gmail.com>
2019-12-17 18:46:33 -0500

    Additional changes to webpack to get it working

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: path.join(__dirname, '/src/app.ts'),
@@ -7,6 +10,11 @@ module.exports = {
         path: path.join(__dirname, '/dist')
     },
     target: 'node',
+    node: {
+      __dirname: false,
+      __filename: false
+    },
+    externals: [nodeExternals()],
     module: {
         rules: [
             {
